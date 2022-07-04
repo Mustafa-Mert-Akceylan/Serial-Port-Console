4bf1ff8f85e6bb38766e85ded49430b64b76e50c
M² <80332192+Mustafa-Mert-Akceylan@users.noreply.github.com>
2022-07-04 18:45:13 +0300

    Add files via upload

---
+++ Arduino Console/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace Arduino_Console
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 288);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(998, 34);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1019, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Gönder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(103, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(248, 35);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Portlar :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bağlantı Durumu :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(189, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 27);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Bağlı Değil";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Bağlan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 34);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Bağlantıyı Kes";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 27);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Baud :";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "300",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "74880",
+            "115200",
+            "230400",
+            "250000",
+            "500000",
+            "1000000",
+            "20000000"});
+            this.comboBox2.Location = new System.Drawing.Point(84, 27);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(352, 35);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1019, 327);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(167, 34);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Konsolu Temizle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1171, 264);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(357, 26);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(187, 35);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Yenile";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(1021, 367);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(165, 31);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Zamanı Göster\r\n";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(15, 328);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(550, 168);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bağlantı";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.maskedTextBox2);
+            this.groupBox2.Controls.Add(this.maskedTextBox1);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Location = new System.Drawing.Point(571, 328);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(442, 168);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Diğer Ayarlar";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(196, 112);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(240, 34);
+            this.maskedTextBox2.TabIndex = 17;
+            this.maskedTextBox2.Visible = false;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(196, 65);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(240, 34);
+            this.maskedTextBox1.TabIndex = 16;
+            this.maskedTextBox1.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(186, 27);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Okuma Gecikmesi :";
+            this.label6.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(184, 27);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Yazma Gecikmesi :";
+            this.label5.Visible = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(1019, 404);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(181, 31);
+            this.checkBox2.TabIndex = 17;
+            this.checkBox2.Text = "Gelişmiş Ayarlar";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1201, 505);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button4;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ Arduino Console/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Arduino_Console
+{
+    public partial class Form1 : Form
+    {
+        private string veri;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void portlar()
+        {
+            string[] portlar = SerialPort.GetPortNames();
+            foreach (string Port in portlar)
+            {
+                comboBox1.Items.Add(Port);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+              
+            portlar();
+            button1.Enabled = false;
+            button3.Enabled = false;
+            textBox1.ReadOnly = true;
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(SerialPort1_DataReceived);
+        }
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            veri = serialPort1.ReadLine();                      
+            this.Invoke(new EventHandler(displayData_event));
+        }
+        private void displayData_event(object sender, EventArgs e)
+        {      
+            switch (cbdurum)
+            {
+                case 1:
+                    textBox1.Text +=DateTime.Now.ToString() + "|  -Aygıt- " + veri + Environment.NewLine;
+                    break;
+                case 0:
+                    textBox1.Text += "-Aygıt- " + veri + Environment.NewLine;
+                    break ;
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null || comboBox2.Text != "")
+            {
+                
+                try
+                {
+                    if(maskedTextBox1.Text != "")
+                    {
+                        serialPort1.WriteTimeout = Convert.ToInt32(maskedTextBox1.Text);
+                    }
+                    else
+                    {
+                        serialPort1.WriteTimeout = -1;
+                    }
+                    if (maskedTextBox2.Text != "")
+                    {
+                        serialPort1.ReadTimeout = Convert.ToInt32(maskedTextBox2.Text);
+                    }
+                    else
+                    {
+                        serialPort1.ReadTimeout = -1;
+                    }
+
+                    int baud = Convert.ToInt32(comboBox2.SelectedItem);
+                    serialPort1.BaudRate = baud;
+                    serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                    serialPort1.Open();
+                    button2.Enabled = false;
+                    button3.Enabled = true;
+                    label3.Text = "Bağlandı";
+                    label3.ForeColor = Color.Green;
+                    button1.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Karta Bağlanılamadı Lütfen Doğru Bağladığınızdan Emin Olun !");
+                }
+            }
+            else
+            {
+                if(comboBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("Lütfen Port Seçiniz !");
+                }
+                else if (comboBox2.SelectedItem == null)
+                {
+                    MessageBox.Show("Lütfen Baud Rate Seçiniz !");
+                }
+            }
+         
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button3.Enabled = false;
+            serialPort1.Close();
+            button1.Enabled = false;
+            label3.Text = "Bağlı Değil !";
+            label3.ForeColor = Color.Red;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox2.Text != "")
+            {
+                switch (cbdurum)
+                {
+                    case 1:
+                        textBox1.Text += DateTime.Now.ToString() + "|  -Siz- " + textBox2.Text + Environment.NewLine;
+                        break;
+                    case 0:
+                        textBox1.Text += "-Siz- " + textBox2.Text + Environment.NewLine;
+                        break ;
+                }
+                serialPort1.Write(textBox2.Text);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Mesaj Girin !");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Focus();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            portlar();
+        }
+        int cbdurum = 0;
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            switch (checkBox1.Checked)
+            {
+                case true: cbdurum = 1;
+                    break;
+                case false:
+                    cbdurum = 0;
+                    break;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int baud = Convert.ToInt32(comboBox2.SelectedItem);
+            serialPort1.BaudRate = baud;
+        }
+      
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (checkBox2.Checked)
+            {
+                case true:
+                    label5.Visible = true;
+                    label6.Visible = true;
+                    maskedTextBox1.Visible = true;
+                    maskedTextBox2.Visible = true;
+                    break;
+                case false:
+                    label5.Visible = false;
+                    label6.Visible = false;
+                    maskedTextBox1.Visible = false;
+                    maskedTextBox2.Visible = false;
+                    break;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            serialPort1.WriteTimeout = Convert.ToInt32(maskedTextBox1.Text);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            serialPort1.ReadTimeout = Convert.ToInt32(maskedTextBox2.Text);
+        }
+    }
+}
